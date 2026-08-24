648c2066b67d6248a4f4d308d12bbe0a97c5276e
WissamFZ <138994594+WissamFZ@users.noreply.github.com>
2026-08-24 14:06:16 +0545

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -3,7 +3,8 @@
 // { src: "photos/princy1.jpg", title: "pretty girl ♡" }
 
 const photos = [
-  // { src: "photos/princy1.jpg", title: "my favorite smile ♡" },
-  // { src: "photos/princy2.jpg", title: "cutest human ever" },
-  // { src: "photos/princy3.jpg", title: "one for the archive 🎀" },
+   {
+    src: "photos/princy1.jpg",
+    title: "my favorite smile ♡"
+  }
 ];
