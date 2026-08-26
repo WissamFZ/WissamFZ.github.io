41fba5b19b043f9b1689d97d7c9c0a51c062b79a
WissamFZ <138994594+WissamFZ@users.noreply.github.com>
2026-08-26 16:59:22 +0545

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -7,5 +7,25 @@ const photos = [
     src: "photos/princy1.jpg",
     title: "Prettiest Eyes ♡",
     note: "okay but those eyes?? ♡"
+  },
+  {
+    src: "photos/princy2.jpg",
+    title: "My Favorite ♡",
+    note: "yeah... you're kinda pretty"
+  },
+  {
+    src: "photos/princy3.jpg",
+    title: "This One >>>",
+    note: "no explanation needed 🎀"
+  },
+  {
+    src: "photos/princy4.jpg",
+    title: "Pretty Girl ♡",
+    note: "how are you even real?"
+  },
+  {
+    src: "photos/princy5.jpg",
+    title: "Another Favorite",
+    note: "adding this one was mandatory"
   }
 ];
